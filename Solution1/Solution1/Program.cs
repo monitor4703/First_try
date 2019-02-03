da9e5b7c28a8cd306a177d9bbcb49b953343749f
monitor4703 <47084219+monitor4703@users.noreply.github.com>
2019-02-03 21:53:58 +0200

    Add files via upload

---
+++ Solution1/Solution1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int T = 0;
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out T))
+            {
+                int[] N = new int[T];
+                for (int i = 0; i < T; i++)
+                {
+                    N[i] = Int32.Parse(Console.ReadLine());
+                }
+                Call_Summ(N);
+                Console.ReadKey();
+            }
+            else
+            {
+                Start();
+            }
+        }
+ 
+        static void Call_Summ(int[] Smth)
+        {
+            for(int i = 0; i < Smth.Length; i++)
+            {
+                Summ(Smth[i]);
+            }
+
+        }
+
+        static void Summ(int count)
+        {
+            int output = 0;
+            for (int i = 1; i < count; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    output += i;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
